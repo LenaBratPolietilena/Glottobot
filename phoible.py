a7e8953e0dad42a9718b3c2963bc8ba35f4ee78f
Lena <144345346+LenaBratPolietilena@users.noreply.github.com>
2024-05-17 00:05:09 +0300

    Update phoible.py
    The final version of the code.

--- phoible.py
+++ phoible.py
@@ -1,6 +1,11 @@
+"""The phoible module lets user get information about phonological inventories of a requested language's dialects
+from PHOIBLE database. It may give away more than one inventory if there are several dialects described in PHOIBLE or
+if the particular dialect is described in several sources.
+"""
+
 import pandas as pd
 
-# Создаём словарь с парами вида "аббревиатура источника: название источника" для красивого вывода
+# This is a dictionary with pairs "source's abbreveation: source's full name" for a beautiful output.
 inventories_dict = {"aa": "Chanard, C. (2006). Systèmes alphabétiques des langues africaines",
                     "ea": "Nikolaev, Dmitry; Andrey Nikulin; and Anton Kukhto. 2015. The database of Eurasian phonological inventories",
                     "er": "Round, Erich R. 2019. Phonemic inventories of Australia (database of 392 languages)",
@@ -11,40 +16,85 @@ inventories_dict = {"aa": "Chanard, C. (2006). Systèmes alphabétiques des lang
                     "upsid": "Maddieson, I., & Precoda, K. (1990). Updating UPSID. UCLA Working Papers in Phonetics"}
 
 
-# Функция, которая выдаёт всю информацию про заправшиваемый язык из базы данных PHOIBLE.
-def get_info(language_name: str):
+def get_dialect_info(dialect_source_info: pd.DataFrame, language_name: str, dialect: str, source: str) -> str:
+    """
+    This function gives information about a phonological inventory of a particular dialect from a particular source.
+    :param dialect_source_info: a DataFrame with information about a particular dialect from a particular source.
+    :param language_name: a name of the requested language.
+    :param dialect: a dialect of the requested language that is being described.
+    :param source: a source the info about the dialect comes from.
+    :return: A phonological inventory of a particular dialect from a particular source. It returns each inventory in 3 ways: a list of phonemes, a list of phonemes with their allophones, a list of phonemes with their features.
+    """
+
+    if dialect == "nan":
+        dialect = "Standard"
+    output_text = f"Phonological system of {language_name} ({dialect}). Source: {inventories_dict[source]}.\n"
+
+    # Output of a list of segments.
+    consonants_list = list(dialect_source_info[dialect_source_info["SegmentClass"] == "consonant"]["Phoneme"])
+    vowels_list = list(dialect_source_info[dialect_source_info["SegmentClass"] == "vowel"]["Phoneme"])
+    output_text += f"List of phonemes:\nConsonants: {', '.join(consonants_list)}.\nVowels: {', '.join(vowels_list)}.\n"
+
+    # Output of a list of segments with their allophones.
+    output_text += "List of phonemes with their allophones:\nConsonants: "
+    consonants_allophones_list = list(dialect_source_info[dialect_source_info["SegmentClass"] == "consonant"]["Allophones"])
+    vowels_allophones_list = list(dialect_source_info[dialect_source_info["SegmentClass"] == "vowel"]["Allophones"])
+
+    for i in range(len(consonants_list)):
+        if consonants_allophones_list[i] == "nan":
+            consonants_allophones_list[i] = consonants_list[i]
+        output_text += f"{consonants_list[i]} /{', '.join(consonants_allophones_list[i].split())}/ "
+    output_text += "\nVowels: "
+
+    for i in range(len(vowels_list)):
+        if vowels_allophones_list[i] == "nan":
+            vowels_allophones_list[i] = vowels_list[i]
+        output_text += f"{vowels_list[i]} /{', '.join(vowels_allophones_list[i].split())}/ "
+    output_text += "\n"
+
+    # Output of a list of segments with their features.
+    output_text += "List of phonemes with their characteristics:\nConsonants:\n"
+
+    for consonant in consonants_list:
+        consonant_row = dialect_source_info[dialect_source_info["Phoneme"] == consonant]
+        output_text += f"{consonant} ({', '.join(consonant_row.columns[consonant_row.isin(['+']).any()].tolist())})\n"
+    output_text += "\nVowels:\n"
+
+    for vowel in vowels_list:
+        vowel_row = dialect_source_info[dialect_source_info["Phoneme"] == vowel]
+        output_text += f"{vowel} ({', '.join(vowel_row.columns[vowel_row.isin(['+']).any()].tolist())})\n"
+    output_text += "\n"
+
+    return output_text
+
+
+def get_info(language_name: str) -> str:
+    """
+    This function gives all the info about the phonology of the requested language. It may give away more than one
+    inventory if there are several dialects described in PHOIBLE or if the particular dialect is described in several
+    sources.
+    :param language_name: a name of the language that is requested. Can be written in any case.
+    :return: The string with the phonology of the requested language. It returns each inventory in 3 ways: a list of phonemes, a list of phonemes with their allophones, a list of phonemes with their features.
+    """
+
     output_text = "PHOIBLE info:\n\n"
 
-    # languages_info -- датафрейм с информацией про все языки, которые есть в PHOIBLE.
+    # languages_info - a dataframe with information about all languages that are described in PHOIBLE.
     languages_info = pd.read_csv('https://raw.githubusercontent.com/phoible/dev/master/data/phoible.csv', dtype=str).applymap(str)
-    # language_name_info -- датафрейм с информацией только про запрашиваемый язык.
+    # language_name_info - a dataframe with information about the requested language only.
     language_name_info = languages_info[languages_info["LanguageName"].str.lower() == language_name.lower()]
 
-    # Создаём список пар2 (диалект языка, источник, из которого мы достаём информацию про данный диалект).
+    # Here we create a list of tuples: (dialect of the language: source we take information from).
     dialect_source_list = []
     for i in range(language_name_info.shape[0]):
         if (language_name_info["SpecificDialect"].iloc[i], language_name_info["Source"].iloc[i]) not in dialect_source_list:
             dialect_source_list.append((language_name_info["SpecificDialect"].iloc[i], language_name_info["Source"].iloc[i]))
 
-    # Достаём информацию про каждую пару (диалект - источник) и сводим в один вывод.
+    # Here we get all info about each (dialect, source) pair and put it in one output.
     for dialect_source in dialect_source_list:
-        # Создаём датафрейм с информацией только про нужный диалект и источник и передаём его в функцию.
-        dialect_source_info = language_name_info[(language_name_info["SpecificDialect"] == dialect_source[0]) & (language_name_info["Source"] == dialect_source[1])]
+        # We create a dataframe only about a particular dialect and source.
+        dialect_source_info = language_name_info[(language_name_info["SpecificDialect"] == dialect_source[0]) &
+                                                 (language_name_info["Source"] == dialect_source[1])]
         output_text += get_dialect_info(dialect_source_info, language_name, dialect_source[0], dialect_source[1])
-
-    return output_text
-
-
-# Функция, выдающая информацию про конкретный диалект.
-def get_dialect_info(dialect_source_info: pd.DataFrame, language_name: str, dialect: str, source: str):
-    # Выводим все три варианта сразу: фонемы; фонемы + аллофоны; фонемы + их характеристики
-    if dialect == "nan":
-        dialect = "Standard"
-    output_text = f"Phonological system of {language_name} ({dialect}). Source: {inventories_dict[source]}.\n\n"
-
-    # Выводим список согласных и гласных сегментов запрашиваемого языка.
-    consonants_list = list(dialect_source_info[dialect_source_info["SegmentClass"] == "consonant"]["Phoneme"])
-    vowels_list = list(dialect_source_info[dialect_source_info["SegmentClass"] == "vowel"]["Phoneme"])
-    output_text += f"Phonemes\nConsonants: {', '.join(consonants_list)}.\nVowels: {', '.join(vowels_list)}.\n\n"
 
     return output_text
